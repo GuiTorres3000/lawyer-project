d1eb7b9339c0881fd6587f7f1b952a7b60b89713
Lucas Caracoci <127033819+Lucas-Caracoci@users.noreply.github.com>
2025-07-22 20:51:29 -0300

    feat:achievement-page (#12)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,15 @@
 import HeroPage from "./hero/page";
+import AchievementPage from "./achievement/page";
 import Whatwedo from "./whatWeDo/page";
 import LawServices from "./lawServices/page";
+
 
 export default function Home() {
   return (
     <div>
       <HeroPage />
       <Whatwedo />
+      <AchievementPage />
       <LawServices />
     </div>
   );
