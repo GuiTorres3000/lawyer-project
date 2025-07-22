90cc964ef52403e337806c5c815fff3f7099f0d3
Lucas Caracoci <lucascaracoci2@gmail.com>
2025-07-22 20:47:59 -0300

    feat:achievement-page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,13 @@
 import Whatwedo from "@/components/whatwedo";
 import HeroPage from "./hero/page";
+import AchievementPage from "./achievement/page";
 
 export default function Home() {
   return (
     <div>
       <HeroPage />
       <Whatwedo />
+      <AchievementPage />
     </div>
   );
 }
